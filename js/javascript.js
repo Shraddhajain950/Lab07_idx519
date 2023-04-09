cee1ec0481df3380a66fabf29a1f16a210aff97f
Shraddhajain950 <sjain83@id.iit.edu>
2023-04-09 12:15:47 -0500

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,7 @@ function init(){
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Shraddha Jain: " + textbox.value);
+    alert("Shraddha: " + textbox.value);
   }
 
   button.addEventListener('click', showMeText);
